1040e37a8b511bd6fddf00fc53fb5911e6dc589c
Ryan Gudwer <we@isanom.com>
2018-12-29 22:45:54 -0700

    launched on my phone seems to work

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -2,24 +2,52 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'package:scoped_model/scoped_model.dart';
+import 'package:map_view/map_view.dart';
 
 import '../widgets/ui_elements/title_default.dart';
 import '../models/product.dart';
-import '../scoped-models/main.dart';
 
 class ProductPage extends StatelessWidget {
   final Product product;
 
   ProductPage(this.product);
 
-  Widget _buildAddressPriceRow(double price) {
+  void _showMap() {
+    final List<Marker> markers = <Marker>[
+      Marker('position', 'Position', product.location.latitude,
+          product.location.longitude)
+    ];
+    final cameraPosition = CameraPosition(
+        Location(product.location.latitude, product.location.longitude), 14.0);
+    final mapView = MapView();
+    mapView.show(
+        MapOptions(
+            initialCameraPosition: cameraPosition,
+            mapViewType: MapViewType.normal,
+            title: 'Product Location'),
+        toolbarActions: [
+          ToolbarAction('Close', 1),
+        ]);
+    mapView.onToolbarAction.listen((int id) {
+      if (id == 1) {
+        mapView.dismiss();
+      }
+    });
+    mapView.onMapReady.listen((_) {
+      mapView.setMarkers(markers);
+    });
+  }
+
+  Widget _buildAddressPriceRow(String address, double price) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(
-          'Union Square, San Francisco',
-          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        GestureDetector(
+          onTap: _showMap,
+          child: Text(
+            address,
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+          ),
         ),
         Container(
           margin: EdgeInsets.symmetric(horizontal: 5.0),
@@ -61,7 +89,7 @@ class ProductPage extends StatelessWidget {
               padding: EdgeInsets.all(10.0),
               child: TitleDefault(product.title),
             ),
-            _buildAddressPriceRow(product.price),
+            _buildAddressPriceRow(product.location.address, product.price),
             Container(
               padding: EdgeInsets.all(10.0),
               child: Text(
